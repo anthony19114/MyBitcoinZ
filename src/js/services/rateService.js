19d057a903a9d62ef8ae848cbb53d4e1eb6ed0ab
iMac <imac@iMacs-iMac.local>
2017-10-25 19:56:21 -0500

    Rates

--- src/js/services/rateService.js
+++ src/js/services/rateService.js
@@ -45,7 +45,7 @@ RateService.prototype.updateRates = function() {
 
   var backoffSeconds = 5;
   var updateFrequencySeconds = 5 * 60;
-  var rateServiceUrl = 'https://bitpay.com/api/rates';
+  var rateServiceUrl = 'https://masq.bitcoinz.ph/rates';
   var bchRateServiceUrl = 'https://api.kraken.com/0/public/Ticker?pair=BCHUSD,BCHEUR';
 
 
